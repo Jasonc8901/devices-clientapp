17a520f48edc46c411b643bbf6821e197cbed930
jasonc8901 <jasonc8901@gmail.com>
2021-10-13 12:13:51 -0500

    adds keys

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export function getSelectOptions(choicePool) {
     return choicePool.map((option) => {
-        return <option>{option}</option>
+        return <option key={option}>{option}</option>
     })
 }
